741f1325af40a2747ad477ad585c2e162e044d62
Bram-Pl <plessersb@outlook.com>
2022-01-21 22:36:27 +0100

    Bier tonen /all

---
+++ storage/framework/views/5fae3c32e58648f109c7746409cb8c53d610fb8f.php
@@ -0,0 +1,7 @@
+
+<?php $__env->startSection('subtitle', 'List Of Beers'); ?>
+<?php $__env->startSection('content'); ?>
+    <h3>This page is the index page.</h3>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\development\HomeBrewery\resources\views/Beer/index.blade.php ENDPATH**/ ?>
